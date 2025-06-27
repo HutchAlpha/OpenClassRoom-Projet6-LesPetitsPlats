21d93014b44fbbeada3764dc67042e85ac2b0fd2
HutchAlpha <qboisset@hotmail.com>
2025-06-27 13:08:16 +0200

    Avancement partie header

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<head>
+    <titre>Bienvenue sur les petits plats</titre>
+    <style src="css/style.css"></style>
+</head>
+
+<header>
+    <img src="img/Logo.svg" alt="Logo officiel de => Les petits plats">
+    <img src="" alt="Image principal du header">
+    <h1>CHERCHEZ PARMI PLUS DE 1500 RECETTES DU QUOTIDIEN,SIMPLES ET DÉLICIEUSES</h1>
+    <div class="search-bar">
+    <input type="text" id="searchInput" placeholder="Rechercher une recette, un ingrédient, ..." />
+    <button onclick="performSearch()">
+        <img src="img/recherche.svg" alt="logo Rechercher">
+    </button>
+    </div>
+</header>
+
+<article></article>
+
+<footer></footer>
+
+</html>
