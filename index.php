5fa830b579341b0de6df0b11a0fd3fbcc4f651d4
HutchAlpha <qboisset@hotmail.com>
2025-06-27 13:51:46 +0200

    Ajout SCSS

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
-
+<html lang="fr">
 <head>
-    <titre>Bienvenue sur les petits plats</titre>
-    <style src="css/style.css"></style>
-</head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenue sur les petits plats</title>
+    <link rel="stylesheet" href="css/style.css">
 
+</head>
 <header>
-    <img src="img/Logo.svg" alt="Logo officiel de => Les petits plats">
-    <img src="" alt="Image principal du header">
+    <img class="logo" src="img/Logo.svg" alt="Logo officiel de => Les petits plats">
+    <img class="imgMain" src="img/PresentationMain.png" alt="Image principal du header">
     <h1>CHERCHEZ PARMI PLUS DE 1500 RECETTES DU QUOTIDIEN,SIMPLES ET DÉLICIEUSES</h1>
     <div class="search-bar">
-    <input type="text" id="searchInput" placeholder="Rechercher une recette, un ingrédient, ..." />
-    <button onclick="performSearch()">
-        <img src="img/recherche.svg" alt="logo Rechercher">
-    </button>
+        <input type="text" id="searchInput" placeholder="Rechercher une recette, un ingrédient, ..." />
+        <button id="Recherchebutton">
+            <img src="img/recherche.svg" alt="logo Rechercher">
+        </button>
     </div>
 </header>
 
