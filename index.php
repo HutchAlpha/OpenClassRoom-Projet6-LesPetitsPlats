e75b0c125f796801c03c622bea3b58e875822f2a
HutchAlpha <qboisset@hotmail.com>
2025-06-27 14:14:58 +0200

    Header pratiquement terminé

--- index.php
+++ index.php
@@ -7,17 +7,23 @@
     <link rel="stylesheet" href="css/style.css">
 
 </head>
-<header>
-    <img class="logo" src="img/Logo.svg" alt="Logo officiel de => Les petits plats">
-    <img class="imgMain" src="img/PresentationMain.png" alt="Image principal du header">
-    <h1>CHERCHEZ PARMI PLUS DE 1500 RECETTES DU QUOTIDIEN,SIMPLES ET DÉLICIEUSES</h1>
-    <div class="search-bar">
-        <input type="text" id="searchInput" placeholder="Rechercher une recette, un ingrédient, ..." />
-        <button id="Recherchebutton">
-            <img src="img/recherche.svg" alt="logo Rechercher">
-        </button>
+<header class="header">
+    <img class="header__logo" src="img/Logo.svg" alt="Logo officiel de Les petits plats">
+    
+    <div class="header__content">
+        <h1 class="header__title">
+            CHERCHEZ PARMI PLUS DE 1500 RECETTES DU QUOTIDIEN, SIMPLES ET DÉLICIEUSES
+        </h1>
+        
+        <div class="header__search-bar">
+            <input type="text" placeholder="Rechercher une recette, un ingrédient, ..." />
+            <button>
+                <img src="img/recherche.svg" alt="Logo rechercher">
+            </button>
+        </div>
     </div>
 </header>
+
 
 <article></article>
 
