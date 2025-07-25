dc93f0519a0770a2a59fb67220699ea7ed439756
HutchAlpha <qboisset@hotmail.com>
2025-07-25 19:40:06 +0200

    Commencement création de l'Article de recette

--- src/main.js
+++ src/main.js
@@ -21,4 +21,56 @@ function Filtre() {
   });
 }
 
+
+function BlockRecette() {
+  let article = document.createElement("article");
+  article.classList.add("");
+
+  //?Div Haut
+  let divHaut = document.createElement("div");
+  divHaut.classList.add("");
+
+  let img = document.createElement("img");
+  img.src = "";
+
+  divHaut.appendChild(img);
+  article.appendChild(divHaut);
+  //?Fin Div Haut
+
+  //?Div Bas
+  let divBas = document.createElement("div");
+  divBas.classList.add("");
+
+  let TitreArticle = document.createElement("h2");
+  TitreArticle.textContent = "";
+
+  //!Présentation Recettes
+  let DRecette = document.createElement("p");
+  DRecette.textContent = "Recette";
+
+  let DescriptionRecette = document.createElement("p");
+  DescriptionRecette.textContent = "";
+
+
+  let DIngredients = document.createElement("p");
+  DIngredients.textContent = "INGREDIENTS ";
+
+  let ListeIngredients = document.createElement("ul");
+  for (i = 0; i < ingredients.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = ingredients[i];
+    ListeIngredients.appendChild(li);
+  }
+
+  divBas.appendChild(DRecette);
+  divBas.appendChild(DescriptionRecette);
+  divBas.appendChild(DIngredients);
+  DIngredients.appendChild(ListeIngredients);
+  //!Fin Présentation Recettes
+
+  divBas.appendChild(TitreArticle);
+  article.appendChild(divBas);
+  //?Fin Div Bas
+  return article;
+}
 Filtre();
