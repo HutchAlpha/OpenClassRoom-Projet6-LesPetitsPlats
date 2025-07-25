486860ed839a39d0159812245d20cba86e1595be
HutchAlpha <qboisset@hotmail.com>
2025-07-25 18:59:55 +0200

    Ajout fonctionnement barre de recherche

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 let BoutonFiltre = document.getElementById("BoutonFiltre");
 let dropdown = document.getElementById("dropdown");
+let searchFiltre = document.getElementById("searchFiltre");
 
-BoutonFiltre.addEventListener("click", function() {
-  dropdown.classList.toggle("hidden");
-});
+function Filtre() {
+  BoutonFiltre.addEventListener("click", function() {
+    dropdown.classList.toggle("hidden");
+  });
+
+  searchFiltre.addEventListener("input", function() {
+    let filter = searchFiltre.value.toLowerCase();
+    let items = dropdown.getElementsByTagName("li");
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i];
+      if (item.textContent.toLowerCase().includes(filter)) {
+        item.style.display = "";
+      } else {
+        item.style.display = "none";
+      }
+    }
+  });
+}
+
+Filtre();
